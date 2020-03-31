563b08a744ea0defac1c9db615482a4889f5bd2a
sanskriti4799 <40361120+sanskriti4799@users.noreply.github.com>
2020-03-31 09:50:45 +0530

    Delete admin.py

--- admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.utils import timezone
-from .models import Quest,Attempt
-# Register your models here.
-
-
-admin.site.register(Quest)
-admin.site.register(Attempt)
